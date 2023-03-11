d82056c6c86d8d76731ed18026edebc9dbb49291
Manoj <manojdograaa@gmail.com>
2023-03-12 01:40:20 +0530

    Removeing various files

--- OnePLus.java
+++
@@ -1,8 +0,0 @@
-
-public class OnePLus {
-
-public static void main(String[] args){
-  System.out.println("This is Manoj");
-}
-
-}
--- mainjava1.java
+++
@@ -1,6 +0,0 @@
-class MainJava1{
-public static void main(String[] args){
- System.out.println("This is test java");
- System.out.println("THis is another edited line");
- }
-}
